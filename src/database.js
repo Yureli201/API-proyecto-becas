35a982851cc28fde464273b4049f49025e8b81ec
Yureli201 <yurelimartinezmartinez@gmail.com>
2025-12-08 15:35:41 -0600

    Integracion variables de entorno

--- src/database.js
+++ src/database.js
@@ -1,10 +1,17 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const uri = process.env.MONGODB_URL
+const dbName = process.env.MONGODB_NAME
 
 mongoose
-  .connect(
-    "mongodb://localhost:27017/becas_db"
-  ) 
-  .then((db) => console.log("DB connected"))
-  .catch((err) => console.log(err));
+  .connect(uri, {dbName} )
+  .then(() => console.log("DB connected"))
+  .catch((err) => {
+    console.error("Error conectando a MongoDB:", err);
+    process.exit(1);
+  });
 
 export default mongoose;
